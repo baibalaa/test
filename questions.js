b933bdd344a3afb87335e81c9ec55c3e24696a09
baibalaa <baibalaamv@gmail.com>
2025-05-18 23:52:50 +0500

    commit

---
+++ questions.js
@@ -0,0 +1,30 @@
+// questions.js
+
+const questions = [
+    {
+      question: "Which team has won the most championship titles?",
+      correct: "BG Sports Club",
+      wrong: [
+        "Club Teenage",
+        "Maziya Sports",
+        "Club Zefrol",
+        "TC Sports Club",
+        "UN Friends",
+        "Lorenzo Sports Club"
+      ]
+    },
+    {
+      question: "Which team won the 2023 Baibalaa championship?",
+      correct: "Huravee Club",
+      wrong: [
+        "Club Eagles",
+        "Hinnavaru Boys",
+        "Guraidhoo Warriors",
+        "Mahibadhoo Stars",
+        "Nilandhoo Heroes",
+        "Foakaidhoo Fighters"
+      ]
+    },
+    // Add more questions here
+  ];
+  
